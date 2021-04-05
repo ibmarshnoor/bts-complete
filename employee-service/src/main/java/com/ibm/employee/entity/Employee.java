bc5f1cca493382af99ad539d9c58ec941ae981e9
unknown <ibmjavanh09@iiht.tech>
2021-04-05 09:08:06 +0530

    Added Junit Test Cases

--- employee-service/src/main/java/com/ibm/employee/entity/Employee.java
+++ employee-service/src/main/java/com/ibm/employee/entity/Employee.java
@@ -1,5 +1,8 @@
 package com.ibm.employee.entity;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
@@ -11,40 +14,53 @@ public class Employee {
 	@NotNull
 	@NotBlank
 	private String name;
+	@Email
 	private String email;
 	@NotNull
+	@Min(1000000000)
+	@Max(9999999999L)
 	private int mobileNumber;
 	@NotNull
 	private TYPE type;
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public int getMobileNumber() {
 		return mobileNumber;
 	}
+
 	public void setMobileNumber(int mobileNumber) {
 		this.mobileNumber = mobileNumber;
 	}
+
 	public TYPE getType() {
 		return type;
 	}
+
 	public void setType(TYPE type) {
 		this.type = type;
 	}
-	
+
 }
