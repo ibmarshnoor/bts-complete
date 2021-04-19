9b041d74def49f799879a370e217093188643ac7
ibmarshnoor <80327696+ibmarshnoor@users.noreply.github.com>
2021-04-19 09:20:33 +0530

    Status Transitions

--- bug-service/src/main/java/com/ibm/bug/service/BugService.java
+++ bug-service/src/main/java/com/ibm/bug/service/BugService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ibm.bug.entity.Bug;
+import com.ibm.bug.entity.STATUS;
 import com.ibm.bug.repo.BugRepository;
 
 @Service
@@ -29,6 +30,66 @@ public class BugService {
 	}
 
 	public void updateBug(Bug bug) {
+		STATUS status=bug.getStatus();
+		Optional <Bug> oldBug=bugRepository.findById(bug.getId());
+		oldBug.ifPresent(oldbug->{
+			STATUS oldstatus=oldbug.getStatus();
+			if(oldstatus==STATUS.NEW) {
+				if(!(status==STATUS.ASSIGNED || status==STATUS.NEW)) {
+					throw new IllegalArgumentException("Status should be assigned");
+				}
+			}
+			if(oldstatus==STATUS.ASSIGNED) {
+				if(!(status==STATUS.OPEN)) {
+					throw new IllegalArgumentException("Status should be opened");
+				}
+			}
+			if(oldstatus==STATUS.OPEN) {
+				if(!(status==STATUS.FIXED)) {
+					throw new IllegalArgumentException("Status should be fixed");
+				}
+			}
+			if(oldstatus==STATUS.DEFERRED) {
+				if(!(status==STATUS.ASSIGNED)) {
+					throw new IllegalArgumentException("Status should be assigned");
+				}
+			}
+			if(oldstatus==STATUS.DUPLICATE) {
+				if(!(status==STATUS.ASSIGNED)) {
+					throw new IllegalArgumentException("Status should be assigned");
+				}
+			}
+			if(oldstatus==STATUS.REJECTED) {
+				if(!(status==STATUS.CLOSED)) {
+					throw new IllegalArgumentException("Status is Closed");
+				}
+			}
+			if(oldstatus==STATUS.NOT_A_BUG) {
+				if(!(status==STATUS.CLOSED)) {
+					throw new IllegalArgumentException("Status is Closed");
+				}
+			}
+			if(oldstatus==STATUS.FIXED) {
+				if(!(status==STATUS.PENDING_RETEST)) {
+					throw new IllegalArgumentException("Status should be Pending-Retest state");
+				}
+			}
+			if(oldstatus==STATUS.PENDING_RETEST) {
+				if(!(status==STATUS.REOPEN)|| (status==STATUS.VERIFIED)) {
+					throw new IllegalArgumentException("Status should be ropened");
+				}
+			}
+			if(oldstatus==STATUS.REOPEN) {
+				if(!(status==STATUS.ASSIGNED)) {
+					throw new IllegalArgumentException("Status should be assigned");
+				}
+			}
+			if(oldstatus==STATUS.VERIFIED) {
+				if(!(status==STATUS.CLOSED)) {
+					throw new IllegalArgumentException("Status should be closed");
+				}
+			}
+		});
 		bugRepository.save(bug);
 	}
 
